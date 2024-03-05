f5ba5f8cd1ceb1dd85b0aa32d2065650efdbfd70
William <us.thanhlong18@gmail.com>
2024-03-05 14:07:19 +0700

    feat(workflow): 🥱 update delete, music/languaes limit and schedule

--- .github/actions/ghcr-clean/delete_workflows.sh
+++ .github/actions/ghcr-clean/delete_workflows.sh
@@ -8,43 +8,22 @@ temp_file="workflow_runs.payload"
 rm -rf $temp_file
 
 echo "Fetching workflows..."
-echo $(gh api /repos/$user/$repo/actions/runs --paginate | jq '{count: .total_count, runs: [.workflow_runs[] | select(.status=="completed") | {id, conclusion}]}') | jq '.' > $temp_file
+echo $(gh api /repos/$user/$repo/actions/runs --paginate | jq '{count: .total_count, runs: [.workflow_runs[] | select(.status=="completed") | {id, conclusion, created_at}]}' | jq -s 'sort_by(.created_at) | reverse | .[:10]') | jq '.' > $temp_file
 
-ids_to_delete=$(cat "$temp_file" | jq -r '.runs | .[] | select((.conclusion!="success") and (.conclusion!="failure")) | .id')
-
-if [ "$ids_to_delete" = "" ]
-then
-	echo "All workflows are exited successfully or failed. Nothing to remove"
-else
-	echo "Removing all the workflows that are not successful or failed..."
-	while read -r line; do
-		id="$line"
-		echo "Processing workflow $id"
-		echo -n | gh api --method DELETE /repos/$user/$repo/actions/runs/$id --input -
-
-		echo "Stale workflow run with ID $id deleted successfully!"
-	done <<< $ids_to_delete
-fi
-
-ids_to_delete=$(cat "$temp_file" | jq -r '.runs | .[] | select((.conclusion=="success") or (.conclusion=="failure")) | .id')
+ids_to_delete=$(cat "$temp_file" | jq -r '.[].id')
 
 if [ "$ids_to_delete" = "" ]
 then
-	echo "No workflows to check for logs or artifacts. Exiting..."
-else
-	echo -e "\nDeleting workflows without logs and artifacts..."
-	while read -r line; do
-		id="$line"
-		echo "Processing workflow $id"
-		artifact_count=$(gh api /repos/$user/$repo/actions/runs/$id/artifacts | jq -r '.total_count')
-		if [ "$artifact_count" = "0" ]
-		then
-			gh api --silent /repos/$user/$repo/actions/runs/$id/logs || \
-				echo -n | gh api --method DELETE /repos/$user/$repo/actions/runs/$id --input - && \
-				echo "Workflow run without logs and artifacts with ID $id deleted successfully!"
-		fi
-	done <<< $ids_to_delete
+	echo "No recent workflows found. Exiting..."
+	exit 0
 fi
+
+echo "Removing the 10 most recent workflows..."
+for id in $ids_to_delete; do
+	echo "Processing workflow $id"
+	echo -n | gh api --method DELETE /repos/$user/$repo/actions/runs/$id --input -
+	echo "Workflow run with ID $id deleted successfully!"
+done
 
 rm -rf $temp_file
 echo -e "\n\nFinished the workflow run cleaning process"
